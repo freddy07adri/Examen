dc90b0e70ce7997145394bc828e2dede8c80981f
freddy07adri <166969498+freddy07adri@users.noreply.github.com>
2024-05-21 20:04:04 -0600

    Add files via upload

---
+++ variables.h
@@ -0,0 +1,13 @@
+#define MAX_STR 60
+#define MAX_REG 1000
+int pos = 0;
+
+ typedef struct PERSON{
+    char cif[MAX_STR];
+    char nombre[MAX_STR];
+    char lastName[MAX_STR];
+    int year;
+
+};
+
+PERSON people [MAX_REG];
